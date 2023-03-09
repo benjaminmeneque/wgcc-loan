319ef2483d66fea4ca3b5bff81f629ccf7e26f5e
Benjamin <benjamin.cornejo.meneque@gmail.com>
2023-03-09 04:36:17 +0000

    add new files

---
+++ loan/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "loan",
+
+    'summary': """
+        WGCC DEVELOPMENT LOAN
+       """,
+
+    'description': """
+       WGCC DEVELOPMENT LOAN
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/loan_amortization_form.xml',
+        'views/menu.xml',
+        'views/loan_application.xml',
+    ],
+}
---
+++ loan/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import loan_application
+from . import loan_amortization
